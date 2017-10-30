eb64c137266aae2c04389deb86a0b20f6140c8c9
Huda Ridwan <kerupuk.lovers@gmail.com>
2017-10-31 00:54:05 +0700

    modyfied backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const request = require("request")
-var bodyParser = require('body-parser')
+// var bodyParser = require('body-parser')
 
 
 app.use((req, res, next) => {
